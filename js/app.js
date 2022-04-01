a48464ffdeda997e2aed2d482e833bba97610e60
Clebson Moura <clebson.sh@gmail.com>
2022-04-01 12:17:14 -0300

    refatoração da função de checar o vencedor

--- js/app.js
+++ js/app.js
@@ -31,46 +31,35 @@ function reiniciar() {
 }
 
 function checarVencedor() {
-  // ganhar com as 3 casas na horizontal
-  if (checarSequencia(quadrados[0], quadrados[1], quadrados[2])) {
-    mudarCorQuadradosVencedor(quadrados[0], quadrados[1], quadrados[2]);
-    jogadorVencedor.textContent = quadrados[0].textContent;
-  }
-
-  if (checarSequencia(quadrados[3], quadrados[4], quadrados[5])) {
-    mudarCorQuadradosVencedor(quadrados[3], quadrados[4], quadrados[5]);
-    jogadorVencedor.textContent = quadrados[3].textContent;
-  }
-
-  if (checarSequencia(quadrados[6], quadrados[7], quadrados[8])) {
-    mudarCorQuadradosVencedor(quadrados[6], quadrados[7], quadrados[8]);
-    jogadorVencedor.textContent = quadrados[6].textContent;
-  }
-  // ganhar com as 3 casas na vertical
-  if (checarSequencia(quadrados[0], quadrados[3], quadrados[6])) {
-    mudarCorQuadradosVencedor(quadrados[0], quadrados[3], quadrados[6]);
-    jogadorVencedor.textContent = quadrados[0].textContent;
-  }
-
-  if (checarSequencia(quadrados[1], quadrados[4], quadrados[7])) {
-    mudarCorQuadradosVencedor(quadrados[1], quadrados[4], quadrados[7]);
-    jogadorVencedor.textContent = quadrados[1].textContent;
-  }
-
-  if (checarSequencia(quadrados[2], quadrados[5], quadrados[8])) {
-    mudarCorQuadradosVencedor(quadrados[2], quadrados[5], quadrados[8]);
-    jogadorVencedor.textContent = quadrados[2].textContent;
-  }
-  // ganhar com as 3 casas na transversal
-  if (checarSequencia(quadrados[0], quadrados[4], quadrados[8])) {
-    mudarCorQuadradosVencedor(quadrados[0], quadrados[4], quadrados[8]);
-    jogadorVencedor.textContent = quadrados[0].textContent;
-  }
-
-  if (checarSequencia(quadrados[2], quadrados[4], quadrados[6])) {
-    mudarCorQuadradosVencedor(quadrados[2], quadrados[4], quadrados[6]);
-    jogadorVencedor.textContent = quadrados[2].textContent;
-  }
+  const sequecias = [
+    // ganhar com as 3 casas na horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // ganhar com as 3 casas na vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // ganhar com as 3 casas na transversal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  sequecias.map((sequencia) => {
+    if (
+      checarSequencia(
+        quadrados[sequencia[0]],
+        quadrados[sequencia[1]],
+        quadrados[sequencia[2]]
+      )
+    ) {
+      mudarCorQuadradosVencedor(
+        quadrados[sequencia[0]],
+        quadrados[sequencia[1]],
+        quadrados[sequencia[2]]
+      );
+      jogadorVencedor.textContent = quadrados[sequencia[0]].textContent;
+    }
+  });
 }
 
 function checarSequencia(quadrado1, quadrado2, quadrado3) {
@@ -83,7 +72,6 @@ function checarSequencia(quadrado1, quadrado2, quadrado3) {
 
 function mudarCorQuadradosVencedor(quadrado1, quadrado2, quadrado3) {
   let quadrados = [quadrado1, quadrado2, quadrado3];
-  console.log(quadrados);
 
   quadrados.map((quadrado) => {
     quadrado.style.backgroundColor = "#628e52";
